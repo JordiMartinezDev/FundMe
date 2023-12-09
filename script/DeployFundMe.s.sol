e4947f6afb87a6fbf6550eb36a62c6def4396dcb
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-09 07:13:23 +0100

    Tests and deploy fixed code

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,9 +6,10 @@ import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe(msg.sender);
+        FundMe fundMe = new FundMe(msg.sender);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
