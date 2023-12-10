81c42e2857563f731a58b5e1ef25cdfe680c23b4
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-10 11:56:39 +0100

    Testing array of funders added

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -10,10 +10,12 @@ import {HelperConfig} from "../script/HelperConfig.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
     HelperConfig public helperConfig;
+    address  USER = makeAddr("user");
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
          (fundMe, helperConfig) = deployFundMe.run();
+         vm.deal(USER,100 ether);
     }
 
     function testMinimumDollarIsFive() public {
@@ -26,6 +28,30 @@ contract FundMeTest is Test {
         //fundMe.fund{value:2500000000000000000000}();  // We send more than the minimum required amount so it should not revert
     }
     
+    function testFundUpdatesFundedDataStructure() public{
+        vm.prank(USER); // we set the next TX msg.sender as USER, so we fake/prank who is sending the transactions
+        fundMe.fund{value:10 ether}(); // we send 1 eth
+        uint amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded,10 ether);
+
+    }
+
+    function testAddsFunderToArrayOfFunders() public{
+        // Testing 1 user funds and is added correctly to the array
+        vm.prank(USER);
+        fundMe.fund{value:1 ether}();
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+        // Testing 2nd user funds and is added correctly
+         vm.prank(USER);
+        fundMe.fund{value:1 ether}();
+        funder = fundMe.getFunder(1);
+        //Testing no other funders are in the array, so expect recvert upon checking more funders
+        assertEq(funder, USER);
+        vm.expectRevert();
+        funder = fundMe.getFunder(2);
+
+    }
 
     function testOwnerIsMsgSender() public {
         assertEq(msg.sender, fundMe.i_owner());
