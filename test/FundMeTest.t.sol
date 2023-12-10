fe27dfb7f1226440914ef0671ace6abfb331bd61
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-10 11:41:14 +0100

    Test fails if not enough ETH added

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "../lib/forge-std/src/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
@@ -19,6 +19,13 @@ contract FundMeTest is Test {
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
+    function testFundFailsWithoutEnoughETH() public{
+        vm.expectRevert();
+        fundMe.fund(); // we don't send any value here
+        //fundMe.fund{value:2500000000000000000000}();  // We send more than the minimum required amount so it should not revert
+    }
+    
 
     function testOwnerIsMsgSender() public {
         assertEq(msg.sender, fundMe.i_owner());
